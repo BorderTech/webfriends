78a9499f011e23d3b92ce3996196dfd37f496337
Chris Davis <christopher.davis1@homeaffairs.gov.au>
2022-11-29 10:16:32 +1100

    Add Strong HTML element tag

---
+++ webfriends-api/src/main/java/com/github/bordertech/webfriends/api/common/tags/TagStrong.java
@@ -0,0 +1,19 @@
+package com.github.bordertech.webfriends.api.common.tags;
+
+import com.github.bordertech.webfriends.api.common.tag.TagType;
+import com.github.bordertech.webfriends.api.element.grouping.HParagraph;
+import com.github.bordertech.webfriends.api.element.grouping.HStrong;
+
+/**
+ * HTML <code>p</code> tag.
+ *
+ * @param <T> the element type
+ */
+public interface TagStrong<T extends HStrong> extends TagType<T> {
+
+	@Override
+	default String getTagName() {
+		return "strong";
+	}
+
+}
---
+++ webfriends-api/src/main/java/com/github/bordertech/webfriends/api/element/grouping/HStrong.java
@@ -0,0 +1,14 @@
+package com.github.bordertech.webfriends.api.element.grouping;
+
+import com.github.bordertech.webfriends.api.common.combo.FlowPalpableWithPhrasingElement;
+import com.github.bordertech.webfriends.api.common.tag.TagType;
+
+/**
+ * Paragraph element.
+ */
+public interface HStrong extends FlowPalpableWithPhrasingElement {
+
+	@Override
+	TagType getTagType();
+
+}
